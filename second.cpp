1a4f59f1b59c5f1df53230467f02e38e618910e5
CnThjin <75308189+CnThjin@users.noreply.github.com>
2021-11-12 20:52:06 +0700

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main() {
     cout << "Hello github" << endl;
     
-    cout << "Clone back to my PC";
+    cout << "Clone back to my PC" << endl;
+    
+    cout << "Co y de conflict ne babe";
 }
